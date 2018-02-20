4e25be3d8919e78a09850e79f68408c6fad47795
James Humphry <git@binecho.co.uk>
2018-02-20 21:20:31 +0000

    Fix naming conventions to match PEP8

--- fields.py
+++ fields.py
@@ -8,48 +8,48 @@ class SQLField:
     def __init__(self, py_type=None, sql_type=None, sql_name=None, nullable=True):
         if py_type is None:
             raise ValueError('Field cannot be instantiated without specifying py_type')
-        self.__py_type__ = py_type
+        self._py_type = py_type
         if sql_type is None:
             raise ValueError('Field cannot be instantiated without specifying sql_type')
-        self.__sqltype__ = sql_type
-        self.__sqlname__ = sql_name
-        self.__nullable__ = nullable
-        self.__name__ = None
-        self.__slotname__ = None
+        self._sql_type = sql_type
+        self._sql_name = sql_name
+        self._nullable = nullable
+        self._name = None
+        self._slot_name = None
 
     def __set_name__(self, owner, name):
-        self.__name__ = name
-        self.__slotname__ = '_' + name
-        if self.__sqlname__ is None:
-            self.__sqlname__ = name
+        self._name = name
+        self._slot_name = '_' + name
+        if self._sql_name is None:
+            self._sql_name = name
 
     def __set__(self, instance, value):
         if value is None:
-            if self.__nullable__:
-                instance.__setattr__(self.__slotname__, None)
+            if self._nullable:
+                instance.__setattr__(self._slot_name, None)
             else:
-                raise ValueError('''Field '{0}' can not be null.'''.format(self.__name__))
+                raise ValueError('''Field '{0}' can not be null.'''.format(self._name))
 
-        elif not isinstance(value, self.__py_type__):
-            raise ValueError('''Field '{0}' can only be set to values of type '{1}'.'''.format(self.__name__, str(self.__py_type__)))
+        elif not isinstance(value, self._py_type):
+            raise ValueError('''Field '{0}' can only be set to values of type '{1}'.'''.format(self._name, str(self._py_type)))
 
         else:
-            instance.__setattr__(self.__slotname__, value)
+            instance.__setattr__(self._slot_name, value)
 
 
     def __get__(self, instance, owner):
         try:
-            return instance.__getattribute__(self.__slotname__)
+            return instance.__getattribute__(self._slot_name)
         except AttributeError:
             return None
 
     def __str__(self):
         return '{0} ({1} {2})'.format(self.__class__.__name__,
-                                      self.__sqlname__,
+                                      self._sql_name,
                                       self.sql_type())
 
     def sql_type(self):
-        return self.__sqltype__
+        return self._sql_type
 
 
 class IntegerField(SQLField):
--- records.py
+++ records.py
@@ -18,9 +18,9 @@ class SQLRecordMetaClass(type):
                 _fields[k] = namespace[k]
 
         namespace['__slots__'] = tuple(slots)
-        namespace['__fields__'] = _fields
-        namespace['__field_count__'] = len(slots)
-        namespace['__tablename__'] = table_name
+        namespace['_fields'] = _fields
+        namespace['_field_count'] = len(slots)
+        namespace['_table_name'] = table_name
 
         return type.__new__(mcs, name, bases, namespace)
 
@@ -32,38 +32,38 @@ class SQLRecord(metaclass=SQLRecordMetaClass):
             setattr(self, i, None)
 
         if args:
-            if len(args) != self.__field_count__:
+            if len(args) != self._field_count:
                 raise ValueError('{0} values required, {1} supplied.'
-                                 .format(self.__field_count__, len(args)))
+                                 .format(self._field_count, len(args)))
 
-            for field, value in zip(self.__fields__.keys(), args):
+            for field, value in zip(self._fields.keys(), args):
                 setattr(self, field, value)
 
         elif kwargs:
             for key, value in kwargs.items():
-                if key not in self.__fields__:
+                if key not in self._fields:
                     raise ValueError('{0} is not a valid attribute name.'.format(key))
                 setattr(self, key, value)
 
     def fields(self):
-        for k in self.__fields__.keys():
-            yield self.__fields__[k]
+        for k in self._fields.keys():
+            yield self._fields[k]
 
     def values(self):
-        for k in self.__fields__.keys():
-            yield self.__getattribute__(self.__fields__[k].__slotname__)
+        for k in self._fields.keys():
+            yield self.__getattribute__(self._fields[k]._slot_name)
 
     def items(self):
-        for k in self.__fields__.keys():
-            yield k, self.__fields__[k]
+        for k in self._fields.keys():
+            yield k, self._fields[k]
 
     def item_values(self):
-        for k in self.__fields__.keys():
-            yield k, self.__getattribute__(self.__fields__[k].__slotname__)
+        for k in self._fields.keys():
+            yield k, self.__getattribute__(self._fields[k]._slot_name)
 
     def __str__(self):
         result = self.__class__.__name__ + ' with fields {\n'
-        for k in self.__fields__.keys():
-            result += k + ' : ' + str(self.__fields__[k]) + ', \n'
+        for k in self._fields.keys():
+            result += k + ' : ' + str(self._fields[k]) + ', \n'
         result += '}'
         return result
