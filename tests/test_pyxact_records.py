0117871a8f41ba602134535e03a653ffd3823f6a
James Humphry <git@binecho.co.uk>
2018-03-20 23:16:25 +0000

    Split SQLRecord tests between database and non-database sets
    Signed-off-by: James Humphry <git@binecho.co.uk>

--- tests/test_pyxact_records.py
+++ tests/test_pyxact_records.py
@@ -1,4 +1,4 @@
-# Test pyxact.records
+# Test pyxact.records for behaviours that don't require a database
 
 import pytest
 import pyxact.fields as fields
@@ -8,77 +8,72 @@ import pyxact.records as records
 from pyxact.dialects import sqliteDialect
 
 @pytest.fixture('module')
-def sample_record(sqlitedb):
-    class SampleRecord(records.SQLRecord, table_name='sample_record'):
+def sample_record_class():
+    class SampleRecord(records.SQLRecord, table_name='sample_record_class'):
         trans_id=fields.ContextIntField(context_used='trans_id')
         flag=fields.BooleanField()
         amount=fields.NumericField(precision=6, scale=2, allow_floats=True)
         narrative=fields.TextField()
         pk=constraints.PrimaryKeyConstraint(column_names=('trans_id'))
 
-    sqlitedb.execute(SampleRecord.create_table_sql())
-
     return SampleRecord
 
 @pytest.fixture('module')
-def linked_record(sqlitedb, sample_record):
-    class LinkedRecord(records.SQLRecord, table_name='linked_record'):
-        record_id=fields.ContextIntField(context_used='record_id')
-        trans_id=fields.ContextIntField(context_used='trans_id')
-        some_data=fields.RealField()
-        pk=constraints.PrimaryKeyConstraint(column_names=('record_id',))
-        fk=constraints.ForeignKeyConstraint(column_names=('trans_id',),
-                                            foreign_table='sample_record')
+def sample_records(sample_record_class):
 
-    sqlitedb.execute(LinkedRecord.create_table_sql())
+    result = []
+    result.append(sample_record_class(1, True, '3.4', 'Line 1'))
+    result.append(sample_record_class(2, False, '1.1', 'Line 2'))
+    result.append(sample_record_class(3, True, '-10.4', 'Line 3'))
+    result.append(sample_record_class(4, False, '2.2', 'Line 4'))
 
-    return LinkedRecord
+    return result
 
-def test_table_creation(sqlitecur, sample_record):
-    sqlitecur.execute('SELECT trans_id, flag, amount, narrative FROM sample_record;')
-    assert [x.sql_name for x in sample_record.fields()] == \
+def test_table_creation(sample_record_class):
+
+    assert [x.sql_name for x in sample_record_class.fields()] == \
                 ['trans_id', 'flag', 'amount', 'narrative']
 
-    assert sample_record().table_name == 'sample_record'
+    assert sample_record_class().table_name == 'sample_record_class'
 
 def test_colliding_field_names():
     with pytest.raises(AttributeError, message='SQLRecordMetaClass should not allow subclasses of SQLRecord'
                                                ' with names that collide with built-in methods/attributes.'):
-        class FailRecord(records.SQLRecord, table_name='sample_record'):
+        class FailRecord(records.SQLRecord, table_name='sample_record_class'):
             ok_name = fields.IntField()
             get_context = fields.IntField()
 
-def test_initialization(sample_record):
-    r1 = sample_record()
+def test_initialization(sample_record_class):
+    r1 = sample_record_class()
     assert all((x is None for x in r1.values()))
 
-    r2 = sample_record(1, True, 3.0, 'test')
+    r2 = sample_record_class(1, True, 3.0, 'test')
     assert set(r2.item_values()) == \
             set((('trans_id', 1), ('flag', True), ('amount', 3.0), ('narrative', 'test')))
 
-    r3 = sample_record(trans_id=1, amount=3.0, flag=True, narrative='test')
+    r3 = sample_record_class(trans_id=1, amount=3.0, flag=True, narrative='test')
     assert set(r3.item_values()) == \
             set((('trans_id', 1), ('flag', True), ('amount', 3.0), ('narrative', 'test')))
 
-    r4 = sample_record(trans_id=1, amount=3.0, narrative='test')
+    r4 = sample_record_class(trans_id=1, amount=3.0, narrative='test')
     assert r4.flag is None
 
     with pytest.raises(ValueError):
-        r5 = sample_record(1, True, 3.0)
+        r5 = sample_record_class(1, True, 3.0)
 
     with pytest.raises(ValueError):
-        r6 = sample_record(trans_id=1, amount=3.0, nonesuch=True, narrative='test')
+        r6 = sample_record_class(trans_id=1, amount=3.0, nonesuch=True, narrative='test')
 
-def test_copy(sample_record):
-    r1 = sample_record(trans_id=1, amount=3.0, flag=True, narrative='test')
+def test_copy(sample_record_class):
+    r1 = sample_record_class(trans_id=1, amount=3.0, flag=True, narrative='test')
     r2 = r1.copy()
     r1.flag = False
     assert r1.flag == False
     assert r2.flag == True
 
-def test_get_set_clear(sample_record):
+def test_get_set_clear(sample_record_class):
 
-    r1 = sample_record(trans_id=1, amount=3.0, flag=True, narrative='test')
+    r1 = sample_record_class(trans_id=1, amount=3.0, flag=True, narrative='test')
     r1.clear()
     assert all((x is None for x in r1.values()))
 
@@ -123,9 +118,9 @@ def test_get_set_clear(sample_record):
     with pytest.raises(ValueError):
         r1.set_values(trans_id=1, amount=3.0, flag=True, narrative='test', nonesuch=3)
 
-def test_fields_values_items(sample_record):
+def test_fields_values_items(sample_record_class):
 
-    r1 = sample_record(trans_id=1, flag=True, amount=3.0, narrative='test')
+    r1 = sample_record_class(trans_id=1, flag=True, amount=3.0, narrative='test')
     field_names = [x._name for x in r1.fields()]
     assert field_names == ['trans_id', 'flag', 'amount', 'narrative']
 
@@ -147,11 +142,13 @@ def test_fields_values_items(sample_record):
     assert field_names == ['trans_id', 'flag', 'amount', 'narrative']
     assert field_values == [4, True, 3.0, 'test']
 
-def test_get_context(sample_record):
-    r1 = sample_record(trans_id=1, flag=True, amount=3.0, narrative='test')
+def test_get_context(sample_record_class):
+    r1 = sample_record_class(trans_id=1, flag=True, amount=3.0, narrative='test')
 
     assert r1.get_context() == {'trans_id' : 1}
 
     r1.trans_id = 3
 
     assert r1.get_context() == {'trans_id' : 3}
+
+
---
+++ tests/test_pyxact_records_sql.py
@@ -0,0 +1,103 @@
+# Test pyxact.records for behaviours that need a sqlite database to demonstrate
+
+import pytest
+import pyxact.fields as fields
+import pyxact.constraints as constraints
+import pyxact.sequences as sequences
+import pyxact.records as records
+from pyxact.dialects import sqliteDialect
+
+@pytest.fixture('module')
+def sample_record_class(sqlitedb):
+    class SampleRecord(records.SQLRecord, table_name='sample_table'):
+        trans_id=fields.ContextIntField(context_used='trans_id')
+        flag=fields.BooleanField()
+        amount=fields.NumericField(precision=6, scale=2, allow_floats=True)
+        narrative=fields.TextField()
+        pk=constraints.PrimaryKeyConstraint(column_names=('trans_id'))
+
+    return SampleRecord
+
+@pytest.fixture('module')
+def sample_records(sample_record_class):
+
+    result = []
+    result.append(sample_record_class(1, True, '3.4', 'Line 1'))
+    result.append(sample_record_class(2, False, '1.1', 'Line 2'))
+    result.append(sample_record_class(3, True, '-10.4', 'Line 3'))
+    result.append(sample_record_class(4, False, '2.2', 'Line 4'))
+
+    return result
+
+@pytest.fixture('module')
+def sample_table(sqlitedb, sample_record_class):
+
+    sqlitedb.execute(sample_record_class.create_table_sql())
+
+    # This will raise an OperationalError if the table doesn't exist or the
+    # names of the columns are wrong
+    sqlitedb.execute('SELECT trans_id, flag, amount, narrative FROM sample_table;')
+
+def test_insert(sample_table, sample_record_class, sample_records, sqlitecur):
+
+    # Ensure the table is empty
+    sqlitecur.execute('DELETE FROM sample_table WHERE 1=1')
+
+    # Insert a single row and make sure it can be read back
+    sqlitecur.execute(sample_record_class.insert_sql(),
+                      sample_records[0].values_sql_repr())
+    sqlitecur.execute('SELECT * FROM sample_table WHERE trans_id=?',
+                      (sample_records[0].trans_id,))
+    assert sqlitecur.fetchone() == (1, 1, 3.4, 'Line 1')
+
+    # Insert a single row using the dangerous string concatenation mathod
+    sqlitecur.execute(sample_records[1].insert_sql_unsafe())
+    sqlitecur.execute('SELECT * FROM sample_table WHERE trans_id=?',
+                      (sample_records[1].trans_id,))
+    assert sqlitecur.fetchone() == (2, 0, 1.1, 'Line 2')
+
+    # Insert the remaining rows in one go and read them back
+    sqlitecur.executemany(sample_record_class.insert_sql(),
+                          [sample_records[2].values_sql_repr(),
+                           sample_records[3].values_sql_repr()])
+    sqlitecur.execute('SELECT * FROM sample_table WHERE trans_id=? OR trans_id=?',
+                   (sample_records[2].trans_id, sample_records[3].trans_id))
+    assert sqlitecur.fetchone() == (3, 1, -10.4, 'Line 3')
+    assert sqlitecur.fetchone() == (4, 0, 2.2, 'Line 4')
+
+    # Check the correct number of rows exist in the table.
+    sqlitecur.execute('SELECT COUNT(*) FROM sample_table;')
+    assert sqlitecur.fetchone() == (4,)
+
+def test_update(sample_table, sample_record_class, sample_records, sqlitecur):
+
+    # Ensure the table is empty
+    sqlitecur.execute('DELETE FROM sample_table WHERE 1=1')
+
+    # Insert all four records
+    for i in sample_records:
+        sqlitecur.execute(i.insert_sql(), i.values_sql_repr())
+
+    # Pick one record and make sure it is stored correctly
+    row = sample_records[2].copy()
+    sqlitecur.execute('SELECT * FROM sample_table WHERE trans_id=?',
+                      (row.trans_id,))
+    assert sqlitecur.fetchone() == (3, 1, -10.4, 'Line 3')
+
+    # Change a field and update the database
+    row.amount = '99.9'
+    sqlitecur.execute(*row.update_sql())
+
+    # Check it has been updated
+    sqlitecur.execute('SELECT * FROM sample_table WHERE trans_id=?',
+                      (row.trans_id,))
+    assert sqlitecur.fetchone() == (3, 1, 99.9, 'Line 3')
+
+    # Now check a different row to make sure it hasn't changed
+    sqlitecur.execute('SELECT * FROM sample_table WHERE trans_id=?',
+                      (1,))
+    assert sqlitecur.fetchone() == (1, 1, 3.4, 'Line 1')
+
+    # Check there are still only four rows
+    sqlitecur.execute('SELECT COUNT(*) FROM sample_table;')
+    assert sqlitecur.fetchone() == (4,)
