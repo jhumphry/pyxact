3457edc9dd9650a4fcf293b7501804f0bdf46c31
James Humphry <git@binecho.co.uk>
2018-04-17 00:03:08 +0100

    Add a more complicated example of a hook
    This example demonstrates the use of query-linked context fields and
    _pre_insert_hook together, along with sub-classing SQLTransaction.
    
    Signed-off-by: James Humphry <git@binecho.co.uk>

--- doc/examples/example_hooks.py
+++ doc/examples/example_hooks.py
@@ -1,8 +1,11 @@
 '''An example of using SQLTransaction hooks.'''
 
+import decimal
+import random
 import sqlite3
 import sys
 
+from pyxact import fields, queries, records, transactions
 from pyxact import loggingdb
 import example_schema
 
@@ -18,6 +21,70 @@ class ReverseTransaction(example_schema.AccountingTransaction):
 # post_select_hook() method which normalizes the data after it has been read in. In this case, it
 # flips the sign of all of the journal amounts. It will inherit all of the SQLField, SQLTable and
 # SQLRecordList attributes from the base class.
+
+SUM_ACCOUNT_QUERY = '''
+SELECT SUM(amount)
+FROM {accounting.journals}
+WHERE account = {account};'''
+
+class SumAccountQuery(queries.SQLQuery, query=SUM_ACCOUNT_QUERY):
+    account = fields.IntField()
+
+class SetAccountTotal(example_schema.AccountingTransaction):
+    account = fields.IntField()
+    desired_sum = fields.NumericField(precision=8, scale=2, allow_floats=True)
+    current_sum = fields.NumericField(precision=8, scale=2, allow_floats=True,
+                                      inexact_quantize=True, query=SumAccountQuery)
+
+    def _pre_insert_hook(self, context, cursor, dialect):
+        super()._pre_insert_hook(context, cursor, dialect)
+
+        if self.transaction is None:
+            self.transaction = example_schema.TransactionTable()
+        if self.journal_list is None:
+            self.journal_list = example_schema.JournalList()
+
+        self.transaction.narrative = 'Adjusting account {} to {}'.format(self.account,
+                                                                         self.desired_sum)
+        self.transaction.creator = 'SYS'
+        self.transaction.t_rev = False
+        posting_needed = self.desired_sum - self.current_sum
+
+        self.journal_list._clear()
+        self.journal_list._append(example_schema.JournalTable(account=self.account, amount=posting_needed))
+        self.journal_list._append(example_schema.JournalTable(account=9999, amount=-posting_needed))
+
+# This more complicated subclass of AccountingTransaction shows how a hook can be used to
+# automatically regenerate some parts of the transaction based on other parts. Here the journals are
+# generated based on context fields provided by the user and a query refreshed from the database,
+# along with the timestamp, tid and row_id fields that are set up to be automatically completed by
+# the standard operation of AccountingTransaction
+
+# Note that the current_sum field has 'inexact_quantize' activated. SQLite does not really support
+# NUMERIC. It stores values as an integer, binary float or text as necessary to avoid losing ,
+# precision but it cannot do decimal fixed-point arithmetic, so uses binary floating-point which
+# will not always exactly quantize down to two decimal places. The 'inexact_quantize' parameter
+# tells the NumericField to silently discard the excess decimal places without complaining.
+
+def generate_transactions(cursor, n=100):
+    '''Add random transactions to the example schema'''
+
+    trans_details = example_schema.TransactionTable(creator='AAA',
+                                                    t_rev=False)
+    trans_journals = example_schema.JournalList(example_schema.JournalTable(),
+                                                example_schema.JournalTable(),
+                                               )
+    tmp_transaction = example_schema.AccountingTransaction(transaction=trans_details,
+                                                           journal_list=trans_journals)
+    for i in range(0,n):
+        tmp_transaction.transaction.narrative='Random transaction '+str(i)
+        x = random.randint(-500, 500)
+        z = decimal.Decimal(x).scaleb(-2)
+        tmp_transaction.journal_list[0].amount = z
+        tmp_transaction.journal_list[0].account = random.randint(1000,1020)
+        tmp_transaction.journal_list[1].amount = -z
+        tmp_transaction.journal_list[1].account = random.randint(1000,1020)
+        tmp_transaction._insert_new(cursor)
 
 if __name__ == '__main__':
 
@@ -47,8 +114,8 @@ if __name__ == '__main__':
 
     rev_trans._insert_new(cursor)
 
-    # As insert_new rather than insert_existing has been used, updated values for tid and creation_ts
-    # will have been used and written back into rev_trans
+    # As insert_new rather than insert_existing has been used, updated values for tid and
+    # creation_ts will have been used and written back into rev_trans
 
     original_trans = example_schema.AccountingTransaction(tid=1)
     original_trans._context_select(cursor)
@@ -62,3 +129,21 @@ if __name__ == '__main__':
 
     print('***Reversed transaction***\n')
     print(new_trans)
+
+    print('\nInserting 100 random transactions\n')
+    generate_transactions(cursor=cursor, n=100)
+
+    account_1010 = SumAccountQuery(account=1010)
+    account_1010._execute(cursor)
+    print('Account 1010 has total value: {}'.format(account_1010._result_singlevalue(cursor)))
+    print('(any spurious decimal places are due to the lack of support for true decimal arithmetic '
+          'on NUMERIC data types in SQLite)\n')
+
+    print('Adjusting to value 3.14\n')
+    set_account_1010 = SetAccountTotal(account=1010,
+                                       desired_sum=decimal.Decimal('3.14'))
+    set_account_1010._insert_new(cursor)
+
+    account_1010._execute(cursor)
+    print('Account 1010 has total value: {}\n'.format(account_1010._result_singlevalue(cursor)))
+
