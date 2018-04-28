3b1b6b9ee38fd2c9eaf64dcc89845190f0f307db
James Humphry <git@binecho.co.uk>
2018-04-28 22:26:05 +0100

    Add util/project_checks.sh to run some basic checks
    Currently looks for missing SPDX identifiers and runs pytest in quiet
    mode.
    
    Signed-off-by: James Humphry <git@binecho.co.uk>

---
+++ util/project_checks.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Copyright 2018, James Humphry
+# This work is released under the ISC license - see LICENSE for details
+# SPDX-License-Identifier: ISC
+
+# This is a place to collect various tests and checks that should always pass
+# Intended to be run with the working directory in the root of the project
+
+echo "Checking for Python files lacking an SPDX identifier:"
+grep -r -L --include='*.py' --exclude-dir=tmp 'SPDX\-License\-Identifier'
+echo
+
+echo "Pytest results:"
+pytest -q tests
