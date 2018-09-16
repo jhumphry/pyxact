68fe2b7717ae39c6fc57cef09aa9eb2130714823
James Humphry <git@binecho.co.uk>
2018-09-16 23:28:45 +0100

    pyxact.psycopg2 no longer needs to be a sub-package
    Signed-off-by: James Humphry <git@binecho.co.uk>

--- pyxact/psycopg2.py
+++ pyxact/psycopg2.py
@@ -1,4 +1,4 @@
-'''This package defines the flavour of SQL used by PostgreSQL and pyscopg2.'''
+'''This module defines the flavour of SQL used by PostgreSQL and pyscopg2.'''
 
 # Copyright 2018, James Humphry
 # This work is released under the ISC license - see LICENSE for details
@@ -6,8 +6,8 @@
 
 import enum
 
-from .. import dialects
-from .. import IsolationLevel
+from . import dialects
+from . import IsolationLevel
 
 class Psycopg2Dialect(dialects.SQLDialect):
     '''This is a singleton class that defines the variant of SQL supported by PostgreSQL and the
